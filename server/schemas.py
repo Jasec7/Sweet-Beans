dd5f3c1ddeb8c9b9676eb1c98918715c25a728ca
Jasec <jasec_7@hotmail.com>
2026-02-24 02:11:37 -0800

    add constraints validation to schemas attributes

--- server/schemas.py
+++ server/schemas.py
@@ -1,23 +1,30 @@
 from config import ma
 from models import User, Store, Bean, Coffee
+from marshmallow import validate, pre_load
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
-        id = ma.auto_field()
-        name = ma.auto_field()
-        password = ma.auto_field(load_only=True)
+        id = ma.auto_field(dump_only=True)
+        name = ma.auto_field(required=True,
+             validate=validate.Length(min=2, max=50, error="Name must be between 2 and 50 characters"))
+        password = ma.auto_field(load_only=True,
+                required=True,
+                validate=validate.Length(min=8, error="Password must be at least 8 characters long"))
 
         class Meta:
              model = User
              load_instance = True
 
-
-
 class StoreSchema(ma.SQLAlchemyAutoSchema):
         
-        id = ma.auto_field()
-        name = ma.auto_field()
-        address = ma.auto_field()
-        phone_number = ma.auto_field()
+        id = ma.auto_field(dump_only=True)
+        name = ma.auto_field(required=True,
+             validate=validate.Length(min=2, max=50, error="Name must be between 2 and 50 characters"))
+        address = ma.auto_field( required=True,
+                validate=validate.Length(min=5, max=200))
+        phone_number = ma.auto_field(required=True,
+                     validate=validate.Regexp(
+                           r'^[\d\-\+\(\)\s]{7,20}$',
+                           error="Phone number must be valid"))
 
         class Meta:
              model = Store
@@ -26,9 +33,18 @@ class StoreSchema(ma.SQLAlchemyAutoSchema):
 
 
 class BeanSchema(ma.SQLAlchemyAutoSchema):
-        id = ma.auto_field()
-        roast = ma.auto_field()
-        origin = ma.auto_field()
+        id = ma.auto_field(dump_only=True)
+        roast = ma.auto_field(required=True,
+                validate=validate.OneOf(
+                ["light", "medium", "dark"],
+                error="Roast must be one of: light, medium, dark"))
+        origin = ma.auto_field(required=True, validate=validate.Length(min=2, max=100))
+
+        @pre_load
+        def normalize_roast(self, data, **kwargs):
+              if "roast" in data and isinstance(data["roast"], str):
+                    data["roast"] = data["roast"].lower().strip()
+              return data
 
         class Meta:
              model = Bean
@@ -36,10 +52,14 @@ class BeanSchema(ma.SQLAlchemyAutoSchema):
 
 
 class CoffeeSchema(ma.SQLAlchemyAutoSchema):
-        id = ma.auto_field()
-        brand = ma.auto_field()
-        presentation = ma.auto_field()
-        price = ma.auto_field()
+        id = ma.auto_field(dump_only=True)
+        brand = ma.auto_field(required=True,
+                validate=validate.Length(min=2, max=50, error="Brand name must be between 2 and 50 characters"))
+        presentation = ma.auto_field(required=True,
+                     validate=validate.OneOf(['Ground','Whole Bean'],
+                     error ="Presentation must be one of: Ground, Whole Bean"))
+        price = ma.auto_field(required=True,
+                validate=validate.Range(min=0.01, error="Price must be greater than 0"))
 
         user_id = ma.auto_field(load_only=True)
         store_id = ma.auto_field(load_only=True)
@@ -51,6 +71,7 @@ class CoffeeSchema(ma.SQLAlchemyAutoSchema):
         class Meta:
              model = Coffee
              load_instance = True
+             include_fk = True
 
 
 class UserWithStoresSchema(ma.SQLAlchemySchema):
