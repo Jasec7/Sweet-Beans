7d0f8a223e3843a287daa2ea9e8632b647785a1b
Jasec <jasec_7@hotmail.com>
2026-02-26 03:02:27 -0800

    update app.py

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@ from flask_restful import Resource
 
 from config import app, db, api
 from models import  User, Store, Bean, Coffee
-from schemas import UserSchema, UserWithStoresSchema, UserWithBeansSchema, UserWithCoffeesSchema
+from schemas import UserSchema, StoreSchema, BeanSchema, CoffeeSchema, UserWithStoresSchema, UserWithBeansSchema, UserWithCoffeesSchema
 
 @app.route('/')
 def index():
@@ -15,7 +15,42 @@ class UserResource(Resource):
 
         return UserSchema(many=True).dump(users), 200
     
+class StoreResource(Resource):
+    def get(self):
+        stores = Store.query.all()
+        
+        return StoreSchema(many=True).dump(stores), 200
+
+    def post(self):
+        pass
+
+class BeanResource(Resource):
+    def get(self):
+        beans = Bean.query.all()
+
+        return BeanSchema(many=True).dump(beans), 200
+
+    def post(self):
+        pass
+
+class CoffeeResource(Resource):
+    def get(self):
+        coffees = Coffee.query.all()
+
+        return CoffeeSchema(many=True).dump(coffees), 200
+
+
+class CoffeeResourceId(Resource):
+    def patch(self, id):
+        pass
+
+    def delete(self, id):
+        pass
+    
 api.add_resource(UserResource,'/users')
+api.add_resource(StoreResource,'/stores')
+api.add_resource(BeanResource,'/beans')
+api.add_resource(CoffeeResource,'/coffees')
 
 
 if __name__ == '__main__':
